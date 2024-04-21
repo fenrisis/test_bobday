c332228c546562515f827970a23dd1b49ad39e63
Leonid3# <antonfenrisis@gmail.com>
2024-04-22 01:11:41 +0200

    indent fix

--- app/routers/data_router.py
+++ app/routers/data_router.py
@@ -7,6 +7,7 @@ import json
 
 router = APIRouter()
 
+
 @router.get("/process-data/", response_model=PostResponse)
 async def process_data():
     config = get_config()
@@ -15,24 +16,21 @@ async def process_data():
     username = config['API']['USERNAME']
     password = config['API']['PASSWORD']
 
-    raw_data = await fetch_data(url_get, username, password)  # Убран дублирующийся вызов
+    raw_data = await fetch_data(url_get, username, password)
     transformed_data = transform_data(raw_data)
-    # Преобразование в dict для сериализации в JSON
     data_dict = {k: v.dict() for k, v in transformed_data.items()}
     status_code = await post_data(url_post, data_dict, username, password)
     with open("output.json", "w") as f:
-        # Сохраняем как JSON строку для корректной сериализации
         json.dump(data_dict, f)
     return {"status_code": status_code, "data_saved": "output.json"}
+
 
 def transform_data(raw_data):
     departments = {}
     for employee_data in raw_data["employees"]:
         dept_id = employee_data["department"]["id"]
-        # Инициализация Employee из словаря
         employee = Employee(**employee_data)
         if dept_id not in departments:
             departments[dept_id] = Department(department_name=employee_data["department"]["department_name"], employees=[])
         departments[dept_id].employees.append(employee)
-    # Возвращаем словарь объектов Department, каждый из которых преобразуется в dict
     return {dept_id: dept for dept_id, dept in departments.items()}
